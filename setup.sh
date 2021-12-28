4b0960f6cdd9685382541775266e9c828881a8d2
LuisenDino <luisendino02@gmail.com>
2021-12-28 15:45:29 -0500

    Agregados archivos instaladores

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
 sudo cp C-Media_Player_config.desktop C-Media_Player.desktop /usr/share/applications
+sudo cp -r C-Media_Player /usr/share/C-Media_Player
+sudo cp C-Media_Player /usr/bin
+sudo cp logo/CmediaPlayer.png /usr/share/pixmaps/CmediaPlayer.png
